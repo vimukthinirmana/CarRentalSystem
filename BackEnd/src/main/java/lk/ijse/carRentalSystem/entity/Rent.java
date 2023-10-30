54bd08d6942570e0ba83771dda633ffe672f5ea4
Vimukthi Nirmana Sanketh <vimukthinirmana64@gmail.com>
2023-10-30 17:43:01 +0530

    content updated

--- BackEnd/src/main/java/lk/ijse/carRentalSystem/entity/Rent.java
+++ BackEnd/src/main/java/lk/ijse/carRentalSystem/entity/Rent.java
@@ -1,14 +1,16 @@
 package lk.ijse.carRentalSystem.entity;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Rent {
     private String rentID;
     private String cost;
     private String description;
     private String driverRequest;
-    private String pickupDate;
-    private String pickUpTime;
-    private String returnDate;
-    private String returnTime;
+    private LocalDate pickUpDate;
+    private LocalTime pickUpTime;
+    private LocalDate returnDate;
+    private LocalTime returnTime;
     private String status;
-
 }
--- BackEnd/src/main/java/lk/ijse/carRentalSystem/entity/User.java
+++ BackEnd/src/main/java/lk/ijse/carRentalSystem/entity/User.java
@@ -4,5 +4,4 @@ public class User {
     private String userName;
     private String password;
     private String role;
-
 }
